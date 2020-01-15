4771e0ca148a1409e04bec1553806786a7d8078b
Axel Stai <staiaxel@gmail.com>
2020-01-15 11:32:12 +0100

    initail upload

---
+++ main.js
@@ -0,0 +1,26 @@
+//functions for adding, subtracting, multiplying and dividing 
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(operator, a, b) {
+
+}
+
+
+console.log(add(2, 4));
+console.log(subtract(2, 4));
+console.log(multiply(2, 4));
+console.log(divide(9, 3));
